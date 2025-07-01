f2a6f97d6902cded932d715ecac39e02e382c593
dagabate <dagtfsm@gmail.com>
2025-07-01 03:16:48 -0400

    Final Portfolio Imported to Git.hub

---
+++ js/projects.js
@@ -0,0 +1,30 @@
+const projects = [
+  {
+    title: "Apple.com Cloning",
+    img: "assets/apple.png",
+    description:
+      "A responsive clone of Apple’s homepage built with HTML, CSS, and Bootstrap",
+    link: "https://dag-abate-apple-clone.netlify.app",
+  },
+
+  {
+    title: "Netflix.com Cloning",
+    img: "assets/netflix2.jpg",
+    description: "Netflix clone with React, CSS and Dynamic API- FetchingLogic",
+    link: "https://dagabate.github.io/Netflix-clone-2025",
+  },
+  {
+    title: "Amazon.com Cloning",
+    img: "assets/amazon2.jpg",
+    description:
+      "Amazon eCommerce clone with React and backend API (Fire-Base), Stripe-Payment.",
+    link: "https://dag-abate-amazon-clone2025.netlify.app",
+  },
+  {
+    title: "Forum Platform",
+    img: "assets/forum.png",
+    description:
+      "Q&A platform with full stack (React + FastAPI) & PostgresSQL.",
+    link: "https://evangadi-forum-d2024.netlify.app",
+  },
+];
